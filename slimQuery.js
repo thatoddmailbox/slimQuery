93f87df0c5fd60ff4d7c8882a7815790a7c749bb
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-01-19 15:39:54 -0500

    .text() and .html() now work!

--- slimQuery.js
+++ slimQuery.js
@@ -3,34 +3,56 @@ window.slimQuery = {
 };
 
 function sqElem(selector) {
-	this.dom = document.querySelectorAll(selector);
+	if (selector !== document) {
+		this.dom = document.querySelectorAll(selector);
+	} else {
+		this.dom = document;
+	}
+	return this;
+}
+
+sqElem.prototype.init = function(selector) {
+	return new sqElem(selector);
 }
 
 sqElem.prototype.text = function(newValue) {
 	if (newValue == undefined) {
-		return this.dom.innerText;
+		return this.dom[0].innerText;
 	}
-	this.dom.innerText = newValue;
+	window.slimQuery.each(this.dom, function() {
+		this.innerText = newValue;
+	});
 	return this;
 };
 
 sqElem.prototype.html = function(newValue) {
 	if (newValue == undefined) {
-		return this.dom.innerHTML;
+		return this.dom[0].innerHTML;
 	}
-	this.dom.innerHTML = newValue;
+	window.slimQuery.each(this.dom, function() {
+		this.innerHTML = newValue;
+	});
 	return this;
 };
 
-window.slimQuery.init = function() {
+sqElem.prototype.ready = function(callback) {
+	//this.dom.events
+	this.dom.addEventListener("DOMContentLoaded", function() {
 
+	});
+};
+
+window.slimQuery.each = function(items, callback) {
+	for (var i = 0; i < items.length; i++) {
+		callback.call(items[i], i);
+	}
 };
 
 window.slimQuery.noConflict = function() {
 	window.slimQuery.noConflictFlag = true;
 };
 
-window.sQ = window.slimQuery.init;
+window.sQ = sqElem.prototype.init;
 if (!window.slimQuery.noConflictFlag) {
 	window.$ = window.sQ;
 	window.jQuery = window.sQ;
