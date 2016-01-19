f69cbef1669966af8893a9d50dd8f7ab27e319fb
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-01-19 16:46:34 -0500

    bootstrap doesn't crash on load! that's progress...

--- slimQuery.js
+++ slimQuery.js
@@ -1,19 +1,33 @@
-window.slimQuery = {
-	noConflictFlag: false,
-	eventMap: {
-		click: "click",
-		ready: "DOMContentLoaded"
-	}
+var slimQuery = function(selector) {
+	return sqElem.prototype.init(selector);
 };
 
+slimQuery.event = {
+	special: {}
+};
+slimQuery.eventMap = {
+	click: "click",
+	ready: "DOMContentLoaded"
+};
+slimQuery.support = {};
+
 function sqElem(selector) {
-	if (selector !== document) {
+	if (typeof selector === "function") {
+		// apparently this is a thing
+		$(document).ready(selector);
+	} else if (selector !== document && selector !== window) {
 		this.dom = document.querySelectorAll(selector);
 	} else {
-		this.dom = [document];
+		this.dom = [selector];
 	}
 	return this;
 }
+
+sqElem.prototype = sqElem.fn = slimQuery.fn = {
+	jquery: "1.11.3", // this is for compatibility
+	noConflictFlag: false,
+	slimquery: "0.1"
+};
 
 sqElem.prototype.init = function(selector) {
 	return new sqElem(selector);
@@ -51,10 +65,11 @@ sqElem.prototype.html = function(newValue) {
 
 sqElem.prototype.on = function(event, callback) {
 	window.slimQuery.each(this.dom, function() {
-		this.addEventListener(window.slimQuery.eventMap[event], function(e) {
+		this.addEventListener(slimQuery.eventMap[event], function(e) {
 			callback.call(e);
 		});
 	});
+	return this;
 };
 
 sqElem.prototype.ready = function(callback) {
@@ -65,18 +80,32 @@ sqElem.prototype.click = function(callback) {
 	return this.on("click", callback);
 };
 
-window.slimQuery.each = function(items, callback) {
+slimQuery.each = function(items, callback) {
 	for (var i = 0; i < items.length; i++) {
 		callback.call(items[i], i);
 	}
 };
 
-window.slimQuery.noConflict = function() {
+slimQuery.extend = function() {
+	// based off of https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+	var retVal = Object(arguments[0]);
+	for (var i = 1; i < arguments.length; i++) { // yes, start at one
+		var src = arguments[i];
+		for (var key in src) {
+			if (src.hasOwnProperty(key)) {
+				retVal[key] = src[key];
+			}
+		}
+	}
+	return retVal;
+};
+
+slimQuery.noConflict = function() {
 	window.slimQuery.noConflictFlag = true;
 };
 
-window.sQ = sqElem.prototype.init;
-if (!window.slimQuery.noConflictFlag) {
+window.sQ = window.slimQuery;
+if (!slimQuery.noConflictFlag) {
 	window.$ = window.sQ;
 	window.jQuery = window.sQ;
 }
