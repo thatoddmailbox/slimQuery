fae39a4dddc03bf52c2f806055a9905e4e838524
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-01-19 20:16:30 -0500

    more bootstrap compat this time some work on modals

--- slimQuery.js
+++ slimQuery.js
@@ -11,10 +11,34 @@ slimQuery.eventMap = {
 };
 slimQuery.support = {};
 
+slimQuery.Event = function(src, props) {
+	if (!(this instanceof slimQuery.Event)) {
+		return new slimQuery.Event( src, props );
+	}
+	if (props) {
+		slimQuery.extend(this, props);
+	}
+	this.src = src;
+};
+
+slimQuery.Event.prototype.isDefaultPrevented = function() {
+	return (this.defaultPrevented||false);
+};
+
+slimQuery.Event.prototype.preventDefault = function() {
+	this.defaultPrevented = true;
+};
+
 function sqElem(selector) {
 	if (typeof selector === "function") {
 		// apparently this is a thing
 		$(document).ready(selector);
+	} else if (selector instanceof HTMLElement) {
+		this.dom = [ selector ];
+	} else if (selector instanceof Array) {
+		this.dom = selector;
+	} else if (selector instanceof NodeList) {
+		this.dom = slimQuery.nodeListToArray(selector);
 	} else if (selector !== document && selector !== window) {
 		this.dom = document.querySelectorAll(selector);
 	} else {
@@ -31,7 +55,17 @@ sqElem.prototype = sqElem.fn = slimQuery.fn = {
 
 sqElem.prototype.init = function(selector) {
 	return new sqElem(selector);
-}
+};
+
+sqElem.prototype.attr = function(propertyName, value) {
+	if (value === undefined) {
+		return (this.dom[0].getAttribute(propertyName)||undefined);
+	}
+	window.slimQuery.each(this.dom, function() {
+		this.setAttribute(propertyName, value);
+	});
+	return this;
+};
 
 sqElem.prototype.css = function(propertyName, value) {
 	if (value === undefined) {
@@ -41,6 +75,27 @@ sqElem.prototype.css = function(propertyName, value) {
 		this.setAttribute("style", (this.getAttribute("style")||"") + propertyName + ":" + value + ";");
 	});
 	return this;
+};
+
+sqElem.prototype.data = function(propertyName, value) {
+	if (value === undefined) {
+		if (this.attr("data-sq-private-" + propertyName) === undefined) {
+			return undefined;
+		}
+		return JSON.parse(this.attr("data-sq-private-" + propertyName));
+	}
+	window.slimQuery.each(this.dom, function() {
+		$(this).attr("data-sq-private-" + propertyName, JSON.stringify(value));
+	});
+	return this;
+};
+
+sqElem.prototype.each = function(callback) {
+	return slimQuery.each(this.dom, callback);
+};
+
+sqElem.prototype.find = function(selector) {
+	return slimQuery.find(selector, this);
 };
 
 sqElem.prototype.text = function(newValue) {
@@ -80,6 +135,11 @@ sqElem.prototype.click = function(callback) {
 	return this.on("click", callback);
 };
 
+sqElem.prototype.trigger = function(event) {
+	// TODO: better event system
+	// it will stay stubbed until there is one
+};
+
 slimQuery.each = function(items, callback) {
 	for (var i = 0; i < items.length; i++) {
 		callback.call(items[i], i);
@@ -100,9 +160,26 @@ slimQuery.extend = function() {
 	return retVal;
 };
 
+slimQuery.find = function(search, start) {
+	if (start === undefined) {
+		start = $("html");
+	}
+	return $(start.dom[0].querySelectorAll(search));
+};
+
 slimQuery.noConflict = function() {
 	window.slimQuery.noConflictFlag = true;
 };
+
+slimQuery.nodeListToArray = function() {
+    var a = [];
+
+    for (var i = 0, len = this.length; i < len; i++) {
+        a[i] = this[i];
+    }
+
+    return a;
+}
 
 window.sQ = window.slimQuery;
 if (!slimQuery.noConflictFlag) {
